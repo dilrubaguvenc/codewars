88362bb263abcee21ff795b2890aa8083d3703bb
Dilruba Guvenc <dilruba.guvenc@gmail.com>
2024-01-22 18:00:59 -0500

    added new codewars challenge

---
+++ 8kyu/you-cant-code-under-pressure-1.js
@@ -0,0 +1,13 @@
+// code as fast as you can! you need to double the integer and return it
+
+// my solution
+
+function doubleInterger(i){
+    i *= 2
+    return i;
+}
+
+
+function doubleInteger(i){
+    return i * 2;
+}
