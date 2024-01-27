5fc9e16f25f7f103f7e8c38f4b727d6a9dd6e35f
Dilruba Guvenc <dilruba.guvenc@gmail.com>
2024-01-26 19:29:37 -0500

    added new codewars challenge

--- 7kyu/looking-for-a-benefactor.js
+++ 7kyu/looking-for-a-benefactor.js
@@ -1,8 +1,8 @@
-// The accounts of the "Fat to Fit Club (FFC)" association are supervised by John as a volunteered accountant. The association is funded through financial donations from generous benefactors. John has a list of the first n donations: [14, 30, 5, 7, 9, 11, 15] He wants to know how much the next benefactor should give to the association so that the average of the first n + 1 donations should reach an average of 30. After doing the math he found 149. He thinks that he could have made a mistake.
+// The accounts of the "Fat to Fit Club (FFC)" association are supervised by John as a volunteered accountant. The association is funded through financial donations from generous benefactors. John has a list of the first n donations: [14, 30, 5, 7, 9, 11, 15] 
+// He wants to know how much the next benefactor should give to the association so that the average of the first n + 1 donations should reach an average of 30. 
+// After doing the math he found 149. He thinks that he could have made a mistake.
 
 // if dons = [14, 30, 5, 7, 9, 11, 15] then new_avg(dons, 30) --> 149
-
-// Could you help him?
 
 
 
@@ -41,4 +41,14 @@
 
 
 // my solution
+const newAvg = (arr, newAvg) => {
+    const newDonation = newAvg * (arr.length + 1) - arr.reduce((acc, curr) => acc + curr, 0);
+    if (newDonation <= 0) throw new Error('Expected New Average is too low');
+    return Math.ceil(newDonation);
+} 
 
+function newAvg(arr, newavg) {
+    const result = newavg * (arr.length + 1) - arr.reduce((a, b) => a + b, 0); 
+    if (result <= 0) throw "Expected New Average is too low"; 
+    return Math.ceil(result);
+}
