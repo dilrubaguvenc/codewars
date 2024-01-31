2392c5c34009b88b72ea9f4b77530dd1606ca9d6
Dilruba Guvenc <dilruba.guvenc@gmail.com>
2024-01-30 20:39:49 -0500

    added new codewars challenge

---
+++ 8kyu/if-you-cant-sleep-just-count-sheep.js
@@ -0,0 +1,18 @@
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+// my solution
+
+var countSheep = function (num){
+    let str = "";
+    for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+    return str;
+}
+
+
+var countSheep = function (num){
+    let murmur = '';
+    for (let i = 1; i <= num; i++) {
+      murmur += `${i} sheep...`;
+    }
+    return murmur;
+}
