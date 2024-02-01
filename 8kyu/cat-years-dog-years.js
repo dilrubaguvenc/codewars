0faf78a98bc5657db3b834059f554e51673cba52
Dilruba Guvenc <dilruba.guvenc@gmail.com>
2024-01-31 21:14:00 -0500

    added new codewars challenge

--- 8kyu/cat-years-dog-years.js
+++ 8kyu/cat-years-dog-years.js
@@ -18,3 +18,42 @@
 // 15 dog years for first year
 // +9 dog years for second year
 // +5 dog years for each year after that
+
+
+// my solution
+
+
+var humanYearsCatYearsDogYears = function(y) {
+    if (y == 1) return [1, 15, 15]
+    if (y == 2) return [2, 24, 24]
+    return [y, (y-2) * 4 + 24, (y-2) * 5 + 24]
+}
+
+
+const humanYearsCatYearsDogYears = (humanYears) => {
+    let catYears = 0;
+    let dogYears = 0;
+    
+    for (let i = 1; i <= humanYears; i++) {
+      if (i === 1) {
+        catYears += 15;
+        dogYears += 15;
+      }
+      else if (i === 2) {
+        catYears += 9;
+        dogYears += 9;
+      }
+      else {
+        catYears += 4;
+        dogYears += 5;
+      }
+    }
+    return [humanYears,catYears,dogYears];
+}
+
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let catYears = humanYears < 2 ? 15 : humanYears < 3 ? 24 : (humanYears - 2) * 4 + 24
+    let dogYears = humanYears < 2 ? 15 : humanYears < 3 ? 24 : (humanYears - 2) * 5 + 24
+  return [humanYears,catYears,dogYears];
+}
