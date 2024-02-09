2509b4de1ff031f5168e327b3e053dfb818909d3
Dilruba Guvenc <dilruba.guvenc@gmail.com>
2024-02-09 13:25:15 -0500

    added new codewars challenge

---
+++ 8kyu/find-the-position.js
@@ -0,0 +1,21 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+
+// my solution 
+
+
+function position(letter){
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    return 'Position of alphabet: ' + (alphabet.indexOf(letter) + 1);
+}
+
+
+function position(letter) {
+    return `Position of alphabet: ${letter.charCodeAt(0) - 96}`
+}
+
+
