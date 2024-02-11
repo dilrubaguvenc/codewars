f7f6235b2d3d3f0564d8a57fd85c7687cafabe74
Dilruba Guvenc <dilruba.guvenc@gmail.com>
2024-02-11 11:21:16 -0500

    added new codewars challenge

---
+++ 8kyu/exclamation-mark-series-1-remove-an-exclamation-mark-from-the-end-of-string.js
@@ -0,0 +1,27 @@
+// Remove an exclamation mark from the end of a string. 
+// For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi!!"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+
+
+// my solution
+
+
+function remove(s) {
+    return s.endsWith('!') ? s.slice(0, -1) : s;
+}
+
+
+function remove (string) {
+    if (string[string.length - 1] === '!') {
+      return string.slice(0, string.length - 1);
+    }
+    return string;
+}
