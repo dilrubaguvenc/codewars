ab32f0243fab3b964ce407aed4c1737a8e364f6d
Dilruba Guvenc <dilruba.guvenc@gmail.com>
2024-02-20 20:20:57 -0500

    added new codewars challenge

---
+++ 8kyu/grasshopper-function-syntax-debugging.js
@@ -0,0 +1,19 @@
+// A student was working on a function and made some syntax mistakes while coding. 
+// Help them find their mistakes and fix them.
+
+
+// original code:
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+
+// my solution
+
+
+const main = (verb, noun) => verb + noun
+
+
+function main (verb, noun) {
+    return verb + noun
+}
