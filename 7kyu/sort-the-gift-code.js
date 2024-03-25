4ae9a5587486b606429ebb7c2e57d22e4403842f
Dilruba Guvenc <dilruba.guvenc@gmail.com>
2024-03-24 20:42:05 -0400

    added new codewars challenge

---
+++ 7kyu/sort-the-gift-code.js
@@ -0,0 +1,13 @@
+// Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+// Examples (Input -- => Output):
+// "abcdef"                      -- => "abcdef"
+// "pqksuvy"                     -- => "kpqsuvy"
+// "zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+
+// my solution
+
+function sortGiftCode(code){
+    return code.split('').sort().join('');
+}
