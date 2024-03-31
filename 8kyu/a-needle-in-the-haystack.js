fd50d6b0e1ec4ee46275d76a8534d6574b7ce0d7
Dilruba Guvenc <dilruba.guvenc@gmail.com>
2024-03-30 20:41:36 -0400

    added new codewars challenge

--- 8kyu/a-needle-in-the-haystack.js
+++ 8kyu/a-needle-in-the-haystack.js
@@ -1,8 +1,4 @@
-// Write a function findNeedle() that takes an array full of junk but containing one "needle"
-
-// After your function finds the needle it should return a message (as a string) that says:
-
-// "found the needle at position " plus the index it found the needle, so:
+// Write a function findNeedle() that takes an array full of junk but containing one "needle". After your function finds the needle it should return a message (as a string) that says: "found the needle at position " plus the index it found the needle, so:
 
 // Example(Input --> Output)
 
