9374cb7fadf51cd3e5b660863bbe005da3f68991
Dilruba Guvenc Lorencio <dilruba.guvenc@gmail.com>
2024-07-17 20:44:05 -0400

    added new solution

--- 8kyu/fake-binary.js
+++ 8kyu/fake-binary.js
@@ -5,3 +5,18 @@
 function fakeBin(x) {
     return x.split('').map(n => n < 5 ? 0 : 1).join('');
 }
+
+
+
+function fakeBin(str){
+    var newStr = "";
+    for(var i=0; i<str.length; i++){
+      if(+str[i] >= 5){
+        newStr += "1"
+      }
+      else{
+        newStr += "0";
+      }
+    }
+    return newStr;
+}
