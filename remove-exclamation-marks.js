827eccee269fd603406bb04af25660755dee26c6
Dilruba Guvenc Lorencio <dilruba.guvenc@gmail.com>
2025-01-13 13:08:30 -0500

    added new codewars challenge

---
+++ remove-exclamation-marks.js
@@ -0,0 +1,8 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+// my solution 
+
+function RemoveExclamationMarks(s) {
+    return s.replaceAll('!', '');
+}
