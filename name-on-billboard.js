ebb29062490084afe36921b0f0db2ef6a309a08b
Dilruba Guvenc Lorencio <dilruba.guvenc@gmail.com>
2025-01-19 13:56:23 -0500

    added another solution

--- name-on-billboard.js
+++ name-on-billboard.js
@@ -15,3 +15,8 @@ function billboard (name, price = 30) {
     }
     return totalCost;
 }
+
+
+function billboard (name, price = 30) {
+    return name.split('').reduce((sum,letter) => sum + price, 0);
+}
