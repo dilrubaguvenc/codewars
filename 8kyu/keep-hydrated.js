7c213cfbad1703ece0706eb4fab70254fcd3b9a7
Dilruba Guvenc Lorencio <dilruba.guvenc@gmail.com>
2025-02-07 14:37:16 -0500

    added codewars challenge

---
+++ 8kyu/keep-hydrated.js
@@ -0,0 +1,15 @@
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+
+// my solution 
+function litres (time) {
+    return Math.floor(time * 0.5)
+}
