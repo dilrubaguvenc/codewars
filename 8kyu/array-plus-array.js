95f00c9a2ebe34e60e172c1a04cfaae00c308aa7
Dilruba Guvenc Lorencio <dilruba.guvenc@gmail.com>
2025-02-27 21:25:37 -0500

    added another solution to problem

--- 8kyu/array-plus-array.js
+++ 8kyu/array-plus-array.js
@@ -7,3 +7,10 @@
 function arrayPlusArray(arr1, arr2) {
     return arr1.concat(arr2).reduce((acc, curr) => acc + curr)
 }
+
+
+
+function arrayPlusArray(arr1, arr2) {
+    let arr = [...arr1, ...arr2];
+    return arr.reduce((a, b) => a + b);
+}
