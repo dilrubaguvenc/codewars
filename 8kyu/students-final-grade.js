ee7be05ccb2de22b07f6d7d8c4396b4cdfb957c0
Dilruba Guvenc Lorencio <dilruba.guvenc@gmail.com>
2025-05-03 18:10:37 -0400

    corrected operators

--- 8kyu/students-final-grade.js
+++ 8kyu/students-final-grade.js
@@ -32,9 +32,9 @@ function finalGrade (exam, projects) {
     switch (true) {
         case (exam > 90 || projects > 10):
             return 100;
-        case (exam > 75 && projects <= 5):
+        case (exam > 75 && projects >= 5):
             return 90;
-        case (exam > 50 && projects <= 2):
+        case (exam > 50 && projects >= 2):
             return 75;
         default: 
             return 0;
